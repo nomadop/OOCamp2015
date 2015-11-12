3012995b0589947ae2a9121fe84946fac504e6b0
nomadop <nomadop@gmail.com>
2015-11-12 13:30:13 +0800

    task2 and task3

--- spec/lengthSpec.js
+++ spec/lengthSpec.js
@@ -1,11 +1,121 @@
 import Length from '../src/length';
 
 describe('Length', function() {
-  describe('compairing two length object with same length', function() {
-    it('should return true', function() {
+  describe('compairing two length object', function() {
+    it('should return true when two length value are same', function() {
       let length1 = new Length(10);
       let length2 = new Length(10);
-      expect(length1.isEqual(length2)).toBeTruthy();
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when two length value are diffrent", function () {
+      let length1 = new Length(10);
+      let length2 = new Length(11);
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('compairing two length object with mm unit', function() {
+    it('should return true when two length value are same', function() {
+      let length1 = new Length(10, 'mm');
+      let length2 = new Length(10, 'mm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when two length value are diffrent", function () {
+      let length1 = new Length(10, 'mm');
+      let length2 = new Length(11, 'mm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('compairing two length object with cm unit', function() {
+    it('should return true when two length value are same', function() {
+      let length1 = new Length(10, 'cm');
+      let length2 = new Length(10, 'cm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when two length value are diffrent", function () {
+      let length1 = new Length(10, 'cm');
+      let length2 = new Length(11, 'cm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('compairing two length object with m unit', function() {
+    it('should return true when two length value are same', function() {
+      let length1 = new Length(10, 'm');
+      let length2 = new Length(10, 'm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when two length value are diffrent", function () {
+      let length1 = new Length(10, 'm');
+      let length2 = new Length(11, 'm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('compairing two length object with diffrent unit', function() {
+    it("should return true when length1 is 10mm and length2 is 1cm", function () {
+      let length1 = new Length(10, 'mm');
+      let length2 = new Length(1, 'cm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return true when length1 is 1m and length2 is 100cm", function () {
+      let length1 = new Length(1, 'm');
+      let length2 = new Length(100, 'cm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return true when length1 is 1m and length2 is 1000mm", function () {
+      let length1 = new Length(1, 'm');
+      let length2 = new Length(1000, 'mm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when length1 is 10cm and length2 is 10m", function () {
+      let length1 = new Length(10, 'cm');
+      let length2 = new Length(10, 'm');
+
+      let result = length1.isEqual(length2);
+
+      expect(result).toBe(false);
     });
   });
 });
--- src/length.js
+++ src/length.js
@@ -1,9 +1,23 @@
 export default class Length {
-  constructor(value) {
+  constructor(value, unit) {
     this.value = value;
+    this.unit = unit;
+  }
+
+  toCommonValue() {
+    switch(this.unit) {
+      case 'mm' :
+          return this.value;
+      case 'cm' :
+          return this.value * 10;
+      case 'm' :
+          return this.value * 1000;
+      default :
+          return this.value;
+    }
   }
 
   isEqual(other) {
-    return this.value === other.value;
+    return this.toCommonValue() === other.toCommonValue();
   }
 }
